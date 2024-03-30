3aac91ffd8535bd6ec2e677acc14c2c95973ac11
Daniel Häll <Danne.01@hotmail.com>
2024-03-30 15:23:47 +0100

    style(lua): Structured files and created init lua file to be called

---
+++ scripts/event.lua
@@ -0,0 +1,14 @@
+print("event.lua")
+return setmetatable({}, {
+    
+
+});
+
+
+-- Goal:
+-- event = require('event.lua') 
+-- event.on("timer", function()
+-- -- Do stuff
+-- end)
+-- 
+-- event.proc("eventName")
---
+++ scripts/test.lua
@@ -0,0 +1,15 @@
+print("test.lua")
+
+local event = require('event')
+
+event.on("foo", function ()
+    print("foo, has happened!");
+end);
+
+
+while true do
+    local arg = io.read("n");
+    if arg == 1 then
+        event.proc("foo");
+    end
+end
