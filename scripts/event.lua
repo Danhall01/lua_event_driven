5eb1091399be92fa7fbdba2acb19f54c59bc5fa0
Daniel Häll <Danne.01@hotmail.com>
2024-03-31 14:48:37 +0200

    feat(): created event template with basic unit test

--- scripts/event.lua
+++ scripts/event.lua
@@ -1,14 +1,32 @@
 print("event.lua")
-return setmetatable({}, {
-    
-
-});
 
 
+local event = {
+    -- callbacks[i].callable
+    -- callbacks[i].enabled
+    callbacks = {},
+
+}
+event.__index = event;
+
+function event:on(eName, func)
+    local item = {
+        callable = func,
+        enabled = true
+    };
+    table.insert(self.callbacks[eName], item);
+end
+function event:trigger(eName)
+
+end
+
+
+
+return setmetatable({}, event);
 -- Goal:
 -- event = require('event.lua') 
 -- event.on("timer", function()
 -- -- Do stuff
 -- end)
 -- 
--- event.proc("eventName")
+-- event.trigger("eventName")
