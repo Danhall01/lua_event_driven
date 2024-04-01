528ca5eb4e7ad04c6fd18b1880fde60fc8ddc7db
Daniel <wraithe01@hotmail.com>
2024-04-02 01:32:16 +0200

    fix(): Test now tests for remove

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,15 +1,23 @@
 print("test.lua")
 
-local event = require('event')
+local event = require('scripts.utility.event')
 
 event:on("foo", function ()
     print("foo, has happened!");
 end);
 
+local off = event:on("foo", function(nr)
+    if nr then
+        print("Event called by number: " .. nr);
+    end
+end)
 
 while true do
     local arg = io.read("n");
     if arg == 1 then
-        event:trigger("foo");
+        event:trigger("foo", arg);
+    end
+    if arg == 2 then
+        off();
     end
 end
