b7400df951f7a8df560fd35f97128f5620d537b9
Daniel <wraithe01@hotmail.com>
2024-04-02 01:17:35 +0200

    feat(): initial event creation

--- scripts/event.lua
+++ scripts/event.lua
@@ -2,31 +2,55 @@ print("event.lua")
 
 
 local event = {
-    -- callbacks[i].callable
-    -- callbacks[i].enabled
-    callbacks = {},
-
+    eventsList = {},
 }
-event.__index = event;
 
-function event:on(eName, func)
+function event:new()
+    self = setmetatable({}, {__index = event});
+    self.eventsList = {};
+    return self;
+end
+function event:clear()
+    self.eventsList = {};
+end
+
+
+function event:on(name, callback)
+    if not self.eventsList[name] then
+        self.eventsList[name] = {};
+    end
     local item = {
-        callable = func,
+        callable = callback,
         enabled = true
-    };
-    table.insert(self.callbacks[eName], item);
+    }
+    table.insert(self.eventsList[name], item);
+
+    return function()
+        self.remove(name, callback);
+    end
 end
-function event:trigger(eName)
+function event:remove(name, callback)
+    for i, _callback in ipairs(self.eventsList[name] or {}) do
+        if _callback == callback then
+            table.remove(self.eventsList[name], i);
+        end
+    end
+end
 
+function event:trigger(name, ...)
+    for _, item in ipairs(self.eventsList[name] or {}) do
+        if item.enabled then
+            item.callable(...);
+        end
+    end
 end
 
 
-
-return setmetatable({}, event);
+return event:new();
 -- Goal:
--- event = require('event.lua') 
--- event.on("timer", function()
+-- event = require('event.lua')
+-- event.on("eventName", function()
 -- -- Do stuff
 -- end)
--- 
+--
 -- event.trigger("eventName")
